e3f4b0c23af5da1e06bc4c1adf017935b6357b59
David Melendez <145343465+dmelnez@users.noreply.github.com>
2024-01-29 14:00:28 +0100

    Add files via upload

---
+++ Ejercicio02.Fechas.Programacion/Ejercicio02.Fechas.Programacion/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Ejercicio02.Fechas.Programacion
+{
+
+
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+
+            // Fecha Actual
+            DateTime fechaActual = DateTime.Now;
+
+            //Dia
+            int dia = fechaActual.Day;
+            Console.WriteLine("DIA: " + dia);
+
+            //Mes
+      
+            int mes = fechaActual.Month;
+            Console.WriteLine("MES: " + mes);
+
+
+            //Hora
+
+            int hora = fechaActual.Hour;
+            Console.WriteLine("HORAS: " + hora);
+
+            //Minutos
+            int minutos = fechaActual.Minute;
+            Console.WriteLine("MINUTOS: " + minutos);
+
+            //Segundos
+            float segundos = fechaActual.Second;
+            Console.WriteLine("SEGUNDOS: " + segundos);
+
+            
+            //Dia de la Semana
+            var diaSemana = fechaActual.DayOfWeek;
+            Console.WriteLine("DIA SEMANA: " + diaSemana);
+            
+
+            //Dia del Anio
+            int diaAnio = fechaActual.DayOfYear;
+            Console.WriteLine("DIA ANIO: " + diaAnio);
+
+
+
+
+
+
+        }
+
+    }
+
+}
